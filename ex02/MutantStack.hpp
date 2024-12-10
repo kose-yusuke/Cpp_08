1c2c4abe6c4605a28418eb3482097149230efe32
yusuke koseki <yusuke188alc@icloud.com>
2024-12-10 16:17:53 +0900

    done ex02

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,14 +6,41 @@
 /*   By: koseki.yusuke <koseki.yusuke@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 13:31:15 by koseki.yusu       #+#    #+#             */
-/*   Updated: 2024/12/09 13:32:18 by koseki.yusu      ###   ########.fr       */
+/*   Updated: 2024/12/10 16:16:02 by koseki.yusu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
 #include <iostream>
 #include <string>
+#include <stack>
+#include <deque>
+#include <list>
 
-class MutantStack{
+template <typename T, typename Container>
 
+class MutantStack : public std::stack<T, Container> 
+{
+    public:
+        typedef typename Container::iterator iterator;
+        typedef typename Container::const_iterator const_iterator;
+        typedef typename Container::reverse_iterator reverse_iterator;
+        typedef typename Container::const_reverse_iterator const_reverse_iterator;
     
+        MutantStack();
+        MutantStack(const MutantStack<T, Container> &other);
+        ~MutantStack();
+        MutantStack<T, Container> &operator=(const MutantStack<T, Container> &other);
+        
+        iterator begin();
+        iterator end();
+        const_iterator begin() const;
+        const_iterator end() const;
+        reverse_iterator rbegin();
+        reverse_iterator rend();
+        const_reverse_iterator rbegin() const;
+        const_reverse_iterator rend() const;
 };
+
+#include "MutantStack.tpp"
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,15 +6,16 @@
 /*   By: koseki.yusuke <koseki.yusuke@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 13:30:20 by koseki.yusu       #+#    #+#             */
-/*   Updated: 2024/12/09 13:32:26 by koseki.yusu      ###   ########.fr       */
+/*   Updated: 2024/12/10 16:17:09 by koseki.yusu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+// std::stack + begin()とかend()とかを自前で実装→MutantStackが通常のSTLコンテナのようになる
 
 int main()
 {
-    MutantStack<int> mstack;
+    MutantStack<int, std::deque<int> > mstack;
     mstack.push(5);
     mstack.push(17);
     std::cout << mstack.top() << std::endl;
@@ -23,10 +24,9 @@ int main()
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
+    MutantStack<int, std::deque<int> >::iterator it = mstack.begin();
+    MutantStack<int, std::deque<int> >::iterator ite = mstack.end();
     ++it;
     --it;
     while (it != ite)
@@ -34,6 +34,39 @@ int main()
     std::cout << *it << std::endl;
     ++it;
     }
-    std::stack<int> s(mstack);
+
+    std::cout << "Reverse iteration:" << std::endl;
+    MutantStack<int, std::deque<int> >::reverse_iterator rit = mstack.rbegin();
+    MutantStack<int, std::deque<int> >::reverse_iterator rite = mstack.rend();
+
+    while (rit != rite) {
+        std::cout << *rit << std::endl;
+        ++rit;
+    }
+
+    std::cout << "Testing with std::list as internal container:" << std::endl;
+    MutantStack<int, std::list<int> > listStack;
+    listStack.push(42);
+    listStack.push(84);
+    listStack.push(126);
+
+    MutantStack<int, std::list<int> >::iterator lit = listStack.begin();
+    MutantStack<int, std::list<int> >::iterator lite = listStack.end();
+
+    while (lit != lite) {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
+
+    std::cout << "Testing stack copy:" << std::endl;
+    MutantStack<int, std::deque<int> > copiedStack = mstack;
+
+    std::cout << "Original stack:" << std::endl;
+    for (MutantStack<int, std::deque<int> >::iterator it = mstack.begin(); it != mstack.end(); ++it)
+        std::cout << *it << std::endl;
+
+    std::cout << "Copied stack:" << std::endl;
+    for (MutantStack<int, std::deque<int> >::iterator it = copiedStack.begin(); it != copiedStack.end(); ++it)
+        std::cout << *it << std::endl;
     return 0;
 }
